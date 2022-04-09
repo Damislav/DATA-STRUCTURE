faa2c20464ffaeea2010106dbe1eade59b7b4b3f
Ivan Damjanovič <idamjanovic28@gmail.com>
2022-04-10 01:53:43 +0200

    second

--- Linked List/script.js
+++ Linked List/script.js
@@ -1,7 +1,29 @@
-class LinkedList {
-  constructor() {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
 }
 
-class Node {
-  constructor() {}
+class LinkedList {
+  constructor(head, size = 0) {
+    this.head = head;
+    this.size = size;
+  }
+
+  //  insert first node
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+  // ¸insert last node
+  // ¸insert at index
+  // get at index
+  // remove at index
+  // ¸print list data
 }
+
+const ll = new LinkedList();
+
+ll.insertFirst(100);
+ll.insertFirst(200);
+console.log(ll);
